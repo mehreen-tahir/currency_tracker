139fafddd71dc653fd6cdd8b5a4ce3a96f7241f9
Mehreen <mehreentahir18@gmail.com>
2020-03-01 16:40:01 +0200

    History: Integerated history saving functionality

--- app/models/history.rb
+++ app/models/history.rb
@@ -2,4 +2,19 @@ class History < ApplicationRecord
   enum currency: [:btc, :eth], _suffix: true
 
   validates :currency, :bid, :ask, :last, :time, presence: true
+  validates :currency, inclusion: { in: currencies.keys }
+
+  scope :ordered_by_time, -> { order(time: :desc) }
+
+  def self.save_latest_prices
+    self.currencies.keys.each do |currency|
+      details = CurrencyService.public_send("fetch_#{currency}")
+      history = History.new(currency: currency, bid: details[:bid], ask: details[:ask], last: details[:last], time: details[:current_time])
+      history.save!
+    end
+
+    true
+    rescue => e
+      false
+  end
 end
