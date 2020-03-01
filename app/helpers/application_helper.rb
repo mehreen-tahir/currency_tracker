227d3e577d8e66dd2419e5dc56f27780b32e4a23
Mehreen <mehreentahir18@gmail.com>
2020-03-01 16:36:50 +0200

    Navigation & Flash messages integerated into app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def alert_class(flash_type)
+    klass_name = case flash_type.to_sym
+    when :success
+      'alert-success'
+    when :alert
+      'alert-danger'
+    end
+
+    "alert #{klass_name} alert-dismissible fade show"
+  end
+
 end
